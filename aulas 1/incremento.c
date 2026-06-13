3104d2d3c23b73d26b3e4cbd4732329b731d63d3
almeidainaciolucas-lgtm <almeidainaciolucas@gmail.com>
2026-06-13 06:14:44 +0000

    começo da aula incremento decremento

---
+++ aulas 1/incremento.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+int numero1 = 1, resultado;
+
+printf("antes incremento %d\n", numero1);
+
+// numero1 = numero1 + 1
+// numero1 += 1
+numero1++;
+printf("apos incremento %d\n", numero1);
+
+//numero1 = numero1 - 1 
+//numero1 -= 1
+numero1--;
+printf("apos dencremento %d\n", numero1);
+
+resultado = numero1++
+printf("apos incremento %d\n", numero1);
+
+
+
+
+
+}
