fa925a203d39e28cc2b7698c2a1a2e0b83704b9a
almeidainaciolucas-lgtm <almeidainaciolucas@gmail.com>
2026-06-13 06:02:39 +0000

    termino de atribuição

--- aulas 1/apenas.c
+++ aulas 1/apenas.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 
 int main (){
-    int numero1, numero2, resultado;
+    int numero1 = 10, numero2 = 2, resultado;
 
     resultado = 10;
-    
     printf("Resultado: %d\n", resultado);
     
+    //resultado = resultado + 20
     resultado += 20;
-
+    printf("Resultado: %d\n", resultado);
+    
+    //resultado = resultado - numero1
+    resultado -= numero1;
     printf("Resultado: %d\n", resultado);
 
+    //resultado = resultado * 5
+    resultado *= 5;
+    printf("resultado: %d\n", resultado);
 
-
-
+    //resultado = resultado / 2
+    resultado /= 2;
+    printf("resultado: %d\n", resultado);
 
 
 
