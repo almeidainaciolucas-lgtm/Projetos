3f0be35c78af2ac8c624667a2f02066e5e5df44a
almeidainaciolucas-lgtm <almeidainaciolucas@gmail.com>
2026-06-15 01:06:37 +0000

    aprendizagem de incremento e decremento

--- aulas 1/incremento.c
+++ aulas 1/incremento.c
@@ -6,21 +6,42 @@ int numero1 = 1, resultado;
 
 printf("antes incremento %d\n", numero1);
 
-// numero1 = numero1 + 1
-// numero1 += 1
+// seria o mesmo numero1 = numero1 + 1
+// seria o mesmo numero1 += 1
+//pos incremento:
+//resultado = numero1
+//numero1++;
+
 numero1++;
 printf("apos incremento %d\n", numero1);
 
-//numero1 = numero1 - 1 
-//numero1 -= 1
+// seria  mesmo numero1 = numero1 - 1 
+//seria o mesmo numero1 -= 1
 numero1--;
-printf("apos dencremento %d\n", numero1);
+printf("apos decremento %d\n", numero1);
 
-resultado = numero1++
-printf("apos incremento %d\n", numero1);
+resultado = numero1++;
+//printf("apos incremento %d\n", numero1);
+printf("apos pós incremento - numero 1: %d - Resultado: %d\n", numero1, resultado);
+
+resultado = ++numero1;
+printf("apos  preincremento - numero 1: %d - Resultado: %d\n", numero1, resultado);
+
+
+resultado = numero1--;
+printf("apos  pos decremento - numero 1: %d - Resultado: %d\n", numero1, resultado);
+
+resultado = --numero1;
+printf("apos  pre decremento - numero 1: %d - Resultado: %d\n", numero1, resultado);
 
 
 
 
+
+
+
+
+
+return 0;
 
 }
