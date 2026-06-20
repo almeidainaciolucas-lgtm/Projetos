126d5554e3c6cb3080dd23ea105eb3f15bcd27c6
almeidainaciolucas-lgtm <almeidainaciolucas@gmail.com>
2026-06-20 00:54:26 +0000

    criaçaõ

---
+++ aulas 1/hora de codar3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    char produto[30] = "produto A";
+    char produto[30] - "produto B";
+
+    unsigned int estoqueA = 1000;
+    unsigned int estoqueB = 2000;
+
+    float valorA = 10.50;
+    float valorB = 20.40;
+
+    unsigned int estoqueMinimoA = 500;
+    unsigned int estoqueMinimoB = 2500;
+
+    double valortotalA;
+    double valortotalB;
+    
+
+
+
+
+
+}
