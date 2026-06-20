3ea12429bca76380b12b2a9b328f5e330982144c
almeidainaciolucas-lgtm <almeidainaciolucas@gmail.com>
2026-06-20 01:24:47 +0000

    aula muito importante para praticar

--- aulas 1/hora de codar3.c
+++ aulas 1/hora de codar3.c
@@ -31,6 +31,7 @@ int main(){
     printf("O produto %s tem estoque minimo %d\n", produtoB, resultadoB);
     //comparações  entre os valores totais dos produtos 
 
+    printf("O valor total de A ( R$ %.2f) é maior que o valor total de B (R$ %.2f): %d\n", estoqueA * valorA, estoqueB * valorB, (estoqueA * valorA) > (estoqueB * valorB));
 
 return 0;
 
